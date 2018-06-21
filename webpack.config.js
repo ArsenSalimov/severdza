a41cb8eb682475d2d3aa905501e6ab879f6e8328
Arsen Salimov <ars.salimov@gmail.com>
2018-06-21 13:17:20 +0300

    Added vue

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // TODO implement seppare configs for different environments
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -8,9 +9,26 @@ module.exports = {
         path: `${__dirname}/build`,
         filename: 'bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html'
-        })
+        }),
+        new VueLoaderPlugin()
     ]
 };
