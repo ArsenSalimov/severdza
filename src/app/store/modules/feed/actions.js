6ee529758324fea294de58d803b38ab478972be8
Arsen Salimov <ars.salimov@gmail.com>
2018-07-25 00:17:12 +0300

    Fix remove api url

--- src/app/store/modules/feed/actions.js
+++ src/app/store/modules/feed/actions.js
@@ -4,7 +4,7 @@ export async function loadFeed({commit}, page = 0) {
     const host = process.env.NODE_ENV == 'production'
         ?  '94.250.249.85'
         : '127.0.0.1';
-    const {data: {items, total}} = await axios.get(`${host}:3000/api/feed?page=${page}`);
+    const {data: {items, total}} = await axios.get(`http://${host}:3000/api/feed?page=${page}`);
     commit('addFeed', {items, total});
 
 }
